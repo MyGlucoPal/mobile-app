dc6af39ec548cb83bf0275b952904873482dd2c2
Hitanshu Dudeja <hitanshududeja80@gmail.com>
2022-01-11 01:37:55 +0530

    Clean up

--- src/DiabetesApp.tsx
+++ src/DiabetesApp.tsx
@@ -1,24 +1,15 @@
 import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
 //Screens
 import HomeScreen from './screens/HomeScreen';
 import AuthScreen from './screens/authentication/AuthScreen';
-// Types
-import type { User } from './@types/commons';
 // Hooks
 import useAuth from './hooks/useAuth';
-
 
 const DiabetesApp = () => {
 
   const { isInitialized, user, isAuthenticated } = useAuth();
-
-  // We want to track if we have a user signed in
-  // 1. If they are not signed in -> then show the login/signup screens
-  // 2. If they are signed in -> We show the main screen of the app
 
   if (!isInitialized){
     return (
@@ -39,22 +30,6 @@ const DiabetesApp = () => {
       <AuthScreen />
     );
   }
-
-  
-  // return (
-  //   <PaperProvider theme={DefaultTheme}>
-  //       {isAuthenticated && user &&
-  //         <HomeScreen />
-  //       }
-  //       {!isAuthenticated && !user && 
-  //         // <LoginScreen />
-  //         <RegisterScreen/>
-
-  //       }
-  //       <Text>This will always be here </Text>
-  //       {/* <RegisterScreen/> */}
-  //   </PaperProvider>
-  // );
 }
 
 const styles = StyleSheet.create({
