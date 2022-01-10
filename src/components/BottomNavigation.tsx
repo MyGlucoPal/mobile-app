f5b513f7c85360dbfe9766661b411ba225d67902
Hitanshu Dudeja <hitanshududeja80@gmail.com>
2022-01-11 02:16:16 +0530

    Clean up

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -1,14 +1,9 @@
 import * as React from 'react';
-import { BottomNavigation as Navigation, Text } from 'react-native-paper';
+import { BottomNavigation as Navigation } from 'react-native-paper';
+
 import HomeScreen from '../screens/HomeScreen';
 import MealScreen from '../screens/MealScreen';
 import UserScreen from '../screens/UserScreen';
-
-// const MusicRoute = () => <Text>Music</Text>;
-
-// const AlbumsRoute = () => <Text>Albums</Text>;
-
-// const RecentsRoute = () => <Text>Recents</Text>;
 
 const BottomNavigation = () => {
   const [index, setIndex] = React.useState(1);
