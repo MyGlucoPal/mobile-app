fa1b1f78bb4c0175806d37c4ea973c971e489060
Carlos Galo <cgalo@squareup.com>
2022-01-16 03:35:18 -0500

    Fixed LoginForm and cleaned AuthScreen

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Text, View } from 'react-native';
 import * as Yup from 'yup';
-import { useFormik, Form, FormikProvider } from 'formik';
-import {DefaultTheme, Button } from 'react-native-paper';
+import { Formik, FormikValues } from 'formik';
+import { DefaultTheme, Button } from 'react-native-paper';
 
 // Custom components
 import Checkbox from './Checkbox';
@@ -25,100 +25,82 @@ const LoginForm = () => {
     const [securePassword, setSecurePassword] = useState(true);
 
     const LoginSchema = Yup.object().shape({
-        email: Yup
-            .string()
-            .label('email')
-            .email('Email must be a valid email address')
-            .required('Email is required'),
-        password: Yup
-            .string()
-            .label('password')
-            .required('Password is required')
-      });
+      email: Yup
+        .string()
+        .label('email')
+        .email('Email must be a valid email address')
+        .required('Email is required'),
+      password: Yup
+        .string()
+        .label('password')
+        .required('Password is required')
+    });
 
-      const formik = useFormik<InitialValues>({
-        initialValues: {
-          email: '',
-          password: '',
-        },
-        validationSchema: LoginSchema,
-        onSubmit: async (values, { setErrors, setSubmitting, resetForm }) => {
-          try {
-            await login(values.email, values.password);
-            // enqueueSnackbar('Login success', {
-            //   variant: 'success',
-            //   action: (key) => (
-            //     <MIconButton size="small" onClick={() => closeSnackbar(key)}>
-            //       {/* <Icon icon={closeFill} /> */}
-            //     </MIconButton>
-            //   )
-            // });
-            if (isMountedRef.current) {
-              setSubmitting(false);
-            }
-          } catch (error: any) {
-            console.error(error);
-            resetForm();
-            if (isMountedRef.current) {
-              setSubmitting(false);
-              setErrors({ afterSubmit: error.message });
-            }
-          }
+    const handleSubmit = async (values:FormikValues) => {
+      try {
+        await login(values.email, values.password);
+        if (isMountedRef.current) {
+          values.setSubmitting(false);
         }
-      });
-
-    const { errors, touched, values, isSubmitting, handleSubmit, getFieldProps, handleChange} = formik;
-
-
+      } catch (error: any) {
+        console.error(error);
+        values.resetForm();
+        if (isMountedRef.current) {
+          values.setSubmitting(false);
+          values.setErrors({ afterSubmit: error.message });
+        }
+      }
+    }
 
     return (
-        <View>
-            <FormikProvider value={formik}>
-                <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
-                    <TextInput
-                        autoCompleteType='username'
-                        textContentType='username'
-                        label="email"
-                        error={Boolean(touched.email && errors.email)}
-                        errorMsg={errors.email}
-                        keyboardType='email-address'
-                        onInput={handleChange('email')}
-                        theme={DefaultTheme}
-                    />
-        
-                    <TextInput
-                        autoCompleteType="password"
-                        textContentType='password'
-                        label="password"
-                        error={Boolean(touched.password && errors.password)}
-                        errorMsg={errors.password}
-                        keyboardType='default'
-                        secureTextEntry={securePassword}
-                        onInput={handleChange('password')}
-                        theme={DefaultTheme}
-                    />
+      <Formik
+        initialValues={{
+          email: '',
+          password: ''
+        } as InitialValues}
+        onSubmit={handleSubmit}
+        validationSchema={LoginSchema}
+      >
+        {({ handleChange, handleSubmit, errors, touched, isSubmitting }) => (
+          <View>
+            <TextInput
+              autoCompleteType='username'
+              textContentType='username'
+              label="email"
+              error={Boolean(touched.email && errors.email)}
+              errorMsg={errors.email}
+              keyboardType='email-address'
+              onInput={handleChange('email')}
+              theme={DefaultTheme}
+            />
+            <TextInput
+              autoCompleteType="password"
+              textContentType='password'
+              label="password"
+              error={Boolean(touched.password && errors.password)}
+              errorMsg={errors.password}
+              keyboardType='default'
+              secureTextEntry={securePassword}
+              onInput={handleChange('password')}
+              theme={DefaultTheme}
+            />
+            <Checkbox 
+              checked={!securePassword}
+              onPress={() => setSecurePassword(!securePassword)}
+              text='Show password'
+            /> 
 
-                    {/* Show password??? */}
-                    
-                    <Checkbox 
-                        checked={!securePassword}
-                        onPress={() => setSecurePassword(!securePassword)}
-                        text='Show password'
-                    /> 
-
-                    <Button 
-                        mode='contained'
-                        loading={isSubmitting}
-                        onPress={handleSubmit}
-                    > 
-                        Submit 
-                    </Button>
-
-                    
-                </Form>
-            </FormikProvider>
+            <Button 
+              mode='contained'
+              loading={isSubmitting}
+              onPress={handleSubmit}
+            >
+              Submit 
+            </Button>
             {errors.afterSubmit && <Text>{errors.afterSubmit}</Text>}
-        </View>
+          </View>
+        )}
+      </Formik>
     );
 }
 
--- src/screens/authentication/AuthScreen.tsx
+++ src/screens/authentication/AuthScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Text, View } from 'react-native';
+import { SafeAreaView, Text, View } from 'react-native';
 
 import RegisterForm from '../../components/RegisterForm';
 import LoginForm from '../../components/LoginForm';
@@ -7,32 +7,21 @@ import { Button } from 'react-native-paper';
 
 const AuthScreen = () => {
     const [isRegistering, setIsRegistering] = useState(true);
-    if (isRegistering){
-        return (
-            <View>
-                <RegisterForm />
-                <Text>
-                    Already have an account?
-                </Text>
-                <Button onPress={()=> setIsRegistering(false) } mode = 'contained'>
-                    Log in
-                </Button>
-            </View>
-        );
-    } else {
-        return (
-            <View>
-                <LoginForm />
-                <Text>
-                    Don't have an account?
-                </Text>
-                <Button onPress={()=> setIsRegistering(true)} mode = 'contained'>
 
-                    Sign up
-                </Button>
-            </View>
-        );
-    }
+    const form = (isRegistering) ? <RegisterForm /> : <LoginForm />;
+    const buttonText = (isRegistering) ? "Register" : "Login"
+
+    return (
+        <SafeAreaView>
+            {form}
+            <Button
+                onPress={() => setIsRegistering(!isRegistering)}
+                mode='outlined'
+            > 
+                {buttonText}
+            </Button>
+        </SafeAreaView>
+    );
 
 }
 export default AuthScreen;
