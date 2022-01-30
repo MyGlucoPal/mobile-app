c82a9799ca32ff53b17856726d0946bfc9a57e06
Carlos Galo <cgalo13@gmail.com>
2022-01-30 16:01:41 -0500

    Cleanup

---
+++ __tests__/components/InfoBox.test.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import Infobox from "../../src/components/InfoBox";
+
+const TEST_TITLE = "Some title";
+const TEST_VALUE = "Some value";
+
+describe("Info box displays correct values", () => {
+  it("has 1 child", () => {
+    const tree = renderer
+      .create(<Infobox title={TEST_TITLE} caption={TEST_VALUE} />)
+      .toJSON();
+    //@ts-ignore
+    expect(tree.children.length).toBe(2);
+  });
+});
+
+it("Info box renders correctly", () => {
+  const tree = renderer
+    .create(<Infobox title={TEST_TITLE} caption={TEST_VALUE} />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
