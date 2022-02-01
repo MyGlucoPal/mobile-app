ce24c33ca7e0707ac906c5826b3bdabf5d8c1952
Carlos Galo <cgalo13@gmail.com>
2022-02-01 00:50:42 -0500

    Updated `theme.ts`

--- src/theme.ts
+++ src/theme.ts
@@ -1,25 +1,32 @@
-import { DefaultTheme } from "react-native-paper";
+import { 
+  DefaultTheme as LightTheme,  
+  DarkTheme
+} from "react-native-paper";
 import type { Theme } from "react-native-paper/lib/typescript/types";
 
 const theme: Theme = {
   dark: false,
-  roundness: DefaultTheme.roundness,
+  roundness: LightTheme.roundness,
   mode: "adaptive",
   colors: {
     primary: "#725AC1",
     background: "#F7ECE1",
     surface: "#CAC4CE",
     accent: "#8D86C9",
-    error: DefaultTheme.colors.error,
-    text: DefaultTheme.colors.text,
+    error: LightTheme.colors.error,
+    text: LightTheme.colors.text,
     onSurface: "#CAC4CE",
-    disabled: DefaultTheme.colors.disabled,
-    placeholder: DefaultTheme.colors.placeholder,
+    disabled: LightTheme.colors.disabled,
+    placeholder: LightTheme.colors.placeholder,
     backdrop: "#242038",
-    notification: "#242038",
+    notification: "#242038"
   },
-  fonts: DefaultTheme.fonts,
-  animation: DefaultTheme.animation,
+  fonts: LightTheme.fonts,
+  animation: LightTheme.animation,
 };
 
-export default theme;
+export {
+  theme,
+  LightTheme,
+  DarkTheme
+};
