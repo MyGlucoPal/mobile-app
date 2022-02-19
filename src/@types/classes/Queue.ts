24eff8c710a6c59386e7bd05a68bce9615e1a35a
Carlos Galo <cgalo13@gmail.com>
2022-02-19 04:19:55 -0500

    Changed public variables to private by using `#`

--- src/@types/classes/Queue.ts
+++ src/@types/classes/Queue.ts
@@ -1,11 +1,11 @@
 export default class Queue <T> {
-    head: Node<T> | null;
-    tail: Node<T> | null;
-    size: number;
+    #head: Node<T> | null;
+    #tail: Node<T> | null;
+    #size: number;
     
     constructor() {
-        this.head = this.tail = null;
-        this.size = 0;
+        this.#head = this.#tail = null;
+        this.#size = 0;
     }
 
     /**
@@ -15,14 +15,14 @@ export default class Queue <T> {
      */
     enqueue(data: T): void {
         var newNode =  new Node(data);
-        if (!this.head || !this.tail){
-            this.head = newNode;
-            this.tail = this.head;
+        if (!this.#head || !this.#tail){
+            this.#head = newNode;
+            this.#tail = this.#head;
         } else {
-            this.tail.nextNode = newNode;
-            this.tail = this.tail.nextNode;
+            this.#tail.nextNode = newNode;
+            this.#tail = this.#tail.nextNode;
         }
-        this.size++;
+        this.#size++;
     }
 
     /**
@@ -31,15 +31,15 @@ export default class Queue <T> {
      * @returns `T` data if queue is not empty, else return undefined
      */
     deque():T | undefined {
-        if (!this.head){
+        if (!this.#head){
             return undefined;
         }
-        var data = this.head.data;
-        this.head = this.head.nextNode;
-        if (this.head == null){
-            this.tail = null;
+        var data = this.#head.data;
+        this.#head = this.#head.nextNode;
+        if (this.#head == null){
+            this.#tail = null;
         }
-        this.size--;
+        this.#size--;
         return data;
     }
 
@@ -49,7 +49,7 @@ export default class Queue <T> {
      * @returns true if the queue is empty
      */
     isEmpty(): boolean {
-        return this.head === null;
+        return this.#head === null;
     }
 
     /**
@@ -57,7 +57,7 @@ export default class Queue <T> {
      * @returns the front element in the queue,
      */
     peek(): T | undefined {
-        return (this.head) ? this.head.data : undefined;
+        return (this.#head) ? this.#head.data : undefined;
     }
 
     /**
@@ -66,7 +66,7 @@ export default class Queue <T> {
      * @returns total ammount of nodes in the queue
      */
     getSize(): number{
-        return this.size;
+        return this.#size;
     }
 }
 
