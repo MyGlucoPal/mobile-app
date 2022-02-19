4fa0d5c6a5e5cce0d1917faf745095c850e8502a
Carlos Galo <cgalo13@gmail.com>
2022-02-19 14:05:19 -0500

    Added error interface

--- src/@types/commons.ts
+++ src/@types/commons.ts
@@ -1,5 +1,9 @@
+import { FirebaseError } from "firebase/app";
 import { Timestamp } from "firebase/firestore";
 
+interface Error extends FirebaseError {}
+
 export {
-    Timestamp
+    Timestamp,
+    Error
 };
