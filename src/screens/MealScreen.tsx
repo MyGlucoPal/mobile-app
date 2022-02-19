852c3cd2e5246e264f341a0d3fe49e1addc7c893
nathanloewe <nathanloewe@gmail.com>
2022-02-19 16:12:11 -0500

    Completed meal screen with food item form

--- src/screens/MealScreen.tsx
+++ src/screens/MealScreen.tsx
@@ -1,23 +1,39 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View } from 'react-native';
 import { Button } from 'react-native-paper';
+
 import useAuth from '../hooks/useAuth';
 
-const MealScreen = () => {
-    const { logout } = useAuth();
+import ItemForm from '../components/meal/ItemForm';
+import FoodItemTable from '../components/meal/FoodItemTable';
 
-    const handleLogout = async() => {
-        try {
-            await logout();
-        } catch (error: any){
-            console.log("Error logging out \n" + error);
-        }
+import { addMeal } from '../services/meal-service';
+
+import { FoodItem, MealType } from '../@types/meals';
+
+const MealScreen = () => {
+    const { user } = useAuth();
+    const [foodItems, setFoodItems] = useState([] as FoodItem[]);
+    const userId = user?.userId || "";
+
+    const onItemAdded=(foodItem: FoodItem) => {
+        setFoodItems(oldFoodItems => [...oldFoodItems, foodItem]);
     }
+
+    const onMealComplete=() => {
+        var allCarbs = 0;
+        for (let i = 0; i < foodItems.length; i++) {
+            allCarbs += foodItems[i].totalCarbs;
+        }
+        
+        addMeal(allCarbs, foodItems, MealType.SNACK, userId);
+    }
+
     return (<View>
-        <Text>Add your meals! </Text>
-        <Button onPress={handleLogout}>
-            Logout
-        </Button>
+        <ItemForm onItemAdded={(foodItem: FoodItem) => {onItemAdded(foodItem)}} />
+        <FoodItemTable foodItems={foodItems}/>
+
+        <Button onPress={onMealComplete}>Register Meal</Button>
     </View>);
 }
 
