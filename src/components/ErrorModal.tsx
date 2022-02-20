c9b3103eb6c2e83317d81a5cd310319a2dd897ff
Carlos Galo <cgalo13@gmail.com>
2022-02-20 04:36:26 -0500

    Created `ErrorModal` to display error messages to user

---
+++ src/components/ErrorModal.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import {
+  Button,
+  Card,
+  Modal,
+  Paragraph,
+  Portal,
+  Title,
+} from "react-native-paper";
+
+import useError from "../hooks/useError";
+
+const ErrorModal = (): JSX.Element => {
+  const { errorMsg, hasError, clearError } = useError();
+
+  return (
+    <Portal>
+      <Modal visible={hasError()} onDismiss={clearError}>
+        <View style={styles.container}>
+          <Card>
+            <Card.Content>
+              <Title>Oh no...</Title>
+              <Paragraph>{errorMsg}</Paragraph>
+            </Card.Content>
+            <Card.Actions style={styles.buttonContainer}>
+              <Button onPress={clearError} mode='contained'>
+                DISMISS
+              </Button>
+            </Card.Actions>
+          </Card>
+        </View>
+      </Modal>
+    </Portal>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 30,
+    marginBottom: 25,
+  },
+  buttonContainer: {
+    alignItems: "center",
+    //   flex-direction: column;
+    flexDirection: "column",
+  },
+});
+
+export default ErrorModal;
