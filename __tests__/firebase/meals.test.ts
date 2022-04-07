edc13e7c42b221e132c8a1ec52a6b0f28b7bcdfa
Carlos Galo <cgalo13@gmail.com>
2022-04-06 22:42:31 -0400

    Finished test verifying user B can't fetch user A meals - Added also some comment blocks with high overview of each test

--- __tests__/firebase/meals.test.ts
+++ __tests__/firebase/meals.test.ts
@@ -1,11 +1,6 @@
 import * as firebase from "@firebase/testing";
-import { Timestamp } from "firebase/firestore";
 
-import { Meal as CreateMealType, MealType } from "../../src/@types/meals";
-
-interface Meal extends CreateMealType {
-  userId: string;
-}
+import { MealType } from "../../src/@types/meals";
 
 const PROJECT_ID = "diabietes-app";
 const myId = "user_abc";
@@ -13,6 +8,28 @@ const theirId = "user_xyz";
 const myAuth = { uid: myId, email: "abc@gmail.com" };
 const theirAuth = { uid: theirId, email: "xyz@gmail.com" };
 const DOC_NAME = "test_doc";
+const THEIR_DUMMY_MEAL = {
+  totalCarbs: 100,
+  totalTimesEaten: 1,
+  dateCreated: new Date(),
+  dateLastModified: new Date(),
+  mealType: MealType.LUNCH,
+  userId: theirId,
+  foodItems: [
+    { name: "banana", totalCarbs: 30, brand: "", servingSize: 1 },
+  ]
+}
+const MY_DUMMY_MEAL = {
+  totalCarbs: 100,
+  totalTimesEaten: 1,
+  dateCreated: new Date(),
+  dateLastModified: new Date(),
+  mealType: MealType.LUNCH,
+  userId: myId,
+  foodItems: [
+    { name: "banana", totalCarbs: 30, brand: "", servingSize: 1 },
+  ]
+}
 
 /**
  * Runs firebase test app against our `PROJECT_ID` with an optional user
@@ -26,8 +43,8 @@ function getFirestore(auth: any) {
 }
 
 /**
- * Similar to `getFirestore()` but returns a FStore that disregards all the security rules
- *
+ * Similar to `getFirestore()` but returns a FStore that disregards all the 
+ * security rules
  */
 function getAdminFirestore() {
   return firebase.initializeAdminApp({ projectId: PROJECT_ID }).firestore();
@@ -50,36 +67,43 @@ describe("Meal security rules", () => {
     await Promise.all(firebase.apps().map((app) => app.delete()));
   });
 
+  /**
+   * Test that verifies that we can fetch our own meals
+   */
   test("We can fetch our personal meals", async () => {
     const db = getFirestore(myAuth);
     const testQuery = db.collection("meals").where("userId", "==", myId);
     await firebase.assertSucceeds(testQuery.get());
   });
 
+  /**
+   * Test verifies that we can fetch a user with `theirId`, can fetch
+   * its meal. 
+   */
   test("We can fetch one meal", async () => {
     const admin = getAdminFirestore();
     const postId = "arbitrary_id";
     const setUpMealDoc = admin.collection("meals").doc(postId);
-    await setUpMealDoc.set({
-      totalCarbs: 100,
-      totalTimesEaten: 1,
-      dateCreated: new Date(),
-      dateLastModified: new Date(),
-      mealType: MealType.LUNCH,
-      userId: theirId,
-      foodItems: [
-        { name: "banana", totalCarbs: 30, brand: "", servingSize: 1 },
-      ],
-    });
+    await setUpMealDoc.set(THEIR_DUMMY_MEAL);
 
     const db = getFirestore(theirAuth);
     const testRead = db.collection("meals").doc(postId);
     await firebase.assertSucceeds(testRead.get());
-    await firebase.clearFirestoreData({ projectId: PROJECT_ID });
   });
 
-
+  /**
+   * Test verifies that user `B` cannot fetch meals from user `A`
+   */
   test("We cannot fetch someone else's meal", async() => {
-    
+    // Create a meal with user `A`
+    const admin = getAdminFirestore();
+    const mealId = "public_post";
+    const setupDoc = admin.collection("meals").doc(mealId);
+    await setupDoc.set(MY_DUMMY_MEAL);
+
+    // Check that no meals are found for user `B`
+    const db = getFirestore(theirAuth);
+    const testRead = db.collection("meals").doc(mealId);
+    await firebase.assertFails(testRead.get());
   });
 });
