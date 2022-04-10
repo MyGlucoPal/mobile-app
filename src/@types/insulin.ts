6a6336f48efb12a956b8454936d35cc0536a134d
Carlos Galo <cgalo13@gmail.com>
2022-04-09 21:41:15 -0400

    Created `insulin.ts` (#37)

---
+++ src/@types/insulin.ts
@@ -0,0 +1,23 @@
+import type { Timestamp } from "../@types/commons";
+
+/**
+ * Insulin dose interface
+ * Note: People with `HIGH` dose level might take an insulin dose even if they don't
+ * have had anything to eat. Therefore, we made `totalCarbs` optional.
+ * 
+ * @param totalInsulinUnits   Total units the user has to intake
+ * @param bloodGlucose        Blood Glucose measurement in milligrams per decilitre (mg/dL)
+ * @param doseLevel           Dose level of the user at the time
+ * @param createdAt           Date/Time this insulin calculation was created
+ * @param totalCarbs          Optional carbs intake used to calculate the `totalInsulinUnits`
+ * @param mealId              Optional meal related to this insuling dose
+ * 
+ */
+export interface InsulinDose {
+   totalInsulinUnits: number;
+   bloodGlucose: number;
+   doseLevel: "LOW" | "MEDIUM" | "HIGH";
+   createdAt?: Timestamp;
+   totalCarbs?: number;
+   mealId?: string;
+}
