3ba03fb62d9e6cc3ef6f44f1e9431e111f8c9e57
nathanloewe <nathanloewe@gmail.com>
2022-04-26 00:33:06 -0400

    Favorite meal modal

--- src/components/meal/FavoriteMealModal.tsx
+++ src/components/meal/FavoriteMealModal.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import {
+    ActivityIndicator,
    Button,
    Card,
+   Colors,
    DefaultTheme,
    Modal,
    Paragraph,
@@ -16,15 +18,24 @@ import { Formik, FormikValues } from 'formik';
 import Checkbox from '../Checkbox';
 import TextInput from '../TextInput';
 
+// Types & interfaces
+import type { FoodItem, Meal, MealType } from '../../@types/meals';
+
+import { addMeal } from '../../services/meal-service';
+
 interface FavoriteMealModalProps {
    isVisible: boolean;
    onDismiss: () => void;
-   onSetMealName: (mealName: string) => void;
+   totalCarbs: number;
+   foodItems: FoodItem[]
+   mealType: MealType;
+   userId: string;
 }
 
 const FavoriteMealModal = (props: FavoriteMealModalProps): JSX.Element => {
    const [inputMealName, setInputMealName] = useState('');
    const [isSavingFavoriteMeal, setIsSavingFavoriteMeal] = useState(false);
+   const [isLoading, setIsLoading] = useState(false);
 
    //    const MealNameSchema = Yup.object().shape({
    //         isFavoriteMeal: Yup.boolean()
@@ -39,55 +50,57 @@ const FavoriteMealModal = (props: FavoriteMealModalProps): JSX.Element => {
    //       password: Yup.string().label('password').required('Password is required'),
    //    });
 
-   function onSubmit() {
-      props.onSetMealName(inputMealName);
+   const onSubmitMeal = () => {
+      addMeal(props.totalCarbs, props.foodItems, props.mealType, props.userId, inputMealName);
+      // We want to send the meal data to the parent component, or send it here directly to firebase
    }
 
-   function onSelectCreateMealName(isSelected: boolean) {
+   const onSelectCreateMealName = (isSelected: boolean) => {
       setIsSavingFavoriteMeal(isSelected);
    }
-
-   // function isMealNameValid():boolean {
-   //    return inputMealName.length >= 50 || inputMealName.length < 1 && isSavingFavoriteMeal && isTextMealNameTouched
-   // }
 
    return (
       <Portal>
          <Modal visible={props.isVisible} onDismiss={props.onDismiss}>
-            <View style={styles.container}>
-               <Card>
-                  <Card.Title title="Save Meal" />
-                  <Card.Content>
-                     <Checkbox
-                        isChecked={onSelectCreateMealName}
-                        text="Do you want to save as favorite meal?"
-                     />
-                     {isSavingFavoriteMeal && (
-                        <TextInput
-                           label="Meal Name"
-                           onInput={setInputMealName}
-                           theme={DefaultTheme}
+            { !isLoading &&
+               <View style={styles.container}>
+                  <Card>
+                     <Card.Title title="Save Meal" />
+                     <Card.Content>
+                        <Checkbox
+                           isChecked={onSelectCreateMealName}
+                           text="Do you want to save as favorite meal?"
                         />
-                     )}
-                  </Card.Content>
-                  <Card.Actions style={styles.buttonContainer}>
-                     <Button
-                        style={styles.button}
-                        onPress={onSubmit}
-                        mode="contained"
-                     >
-                        Submit
-                     </Button>
-                     <Button
-                        style={styles.button}
-                        onPress={() => {}}
-                        mode="contained"
-                     >
-                        Cancel
-                     </Button>
-                  </Card.Actions>
-               </Card>
-            </View>
+                        {isSavingFavoriteMeal && (
+                           <TextInput
+                              label="Meal Name"
+                              onInput={setInputMealName}
+                              theme={DefaultTheme}
+                           />
+                        )}
+                     </Card.Content>
+                     <Card.Actions style={styles.buttonContainer}>
+                        <Button
+                           style={styles.button}
+                           onPress={onSubmitMeal}
+                           mode="contained"
+                        >
+                           Submit
+                        </Button>
+                        <Button
+                           style={styles.button}
+                           onPress={() => {}}
+                           mode="contained"
+                        >
+                           Cancel
+                        </Button>
+                     </Card.Actions>
+                  </Card>
+               </View>
+            }
+            {isLoading && 
+               <ActivityIndicator animating={true} color={Colors.purple800} />
+            }
          </Modal>
       </Portal>
    );
