42c66874128d225a058fe2a22d3366ec7da76a06
EllisD1925 <30882296+EllisD1925@users.noreply.github.com>
2022-04-25 23:40:11 -0400

    Favorite Meal Modal component (#46)
    * Added faorite meal modal, allows for option to take in meal name.
    
    Co-authored-by: hdudeja24 <hdudeja24@users.noreply.github.com>
    Co-authored-by: Carlos Galo <cgalo@users.noreply.github.com>
    
    * Updated checkbox component
    
    * Ran yarn format
    
    Co-authored-by: nathanloewe <nathanloewe@gmail.com>
    Co-authored-by: hdudeja24 <hdudeja24@users.noreply.github.com>
    Co-authored-by: Carlos Galo <cgalo@users.noreply.github.com>

--- src/screens/MealScreen.tsx
+++ src/screens/MealScreen.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import { Button } from 'react-native-paper';
 
 import useAuth from '../hooks/useAuth';
 
 import ItemForm from '../components/meal/ItemForm';
 import FoodItemTable from '../components/meal/FoodItemTable';
+import FavoriteMealModal from '../components/meal/FavoriteMealModal';
 
 import { addMeal } from '../services/meal-service';
 
@@ -14,6 +15,9 @@ import { FoodItem, MealType } from '../@types/meals';
 const MealScreen = () => {
    const { user } = useAuth();
    const [foodItems, setFoodItems] = useState([] as FoodItem[]);
+   const [favoriteMealModalVisible, setFavoriteMealModalVisible] =
+      useState(false);
+   const [currentMealName, setCurrentMealName] = useState('');
    const userId = user?.userId || '';
 
    const onItemAdded = (foodItem: FoodItem) => {
@@ -26,17 +30,29 @@ const MealScreen = () => {
          allCarbs += foodItems[i].totalCarbs;
       }
 
+      setFavoriteMealModalVisible(true);
+
       addMeal(allCarbs, foodItems, MealType.SNACK, userId);
    };
 
    return (
       <View>
+         <FavoriteMealModal
+            isVisible={favoriteMealModalVisible}
+            onDismiss={() => setFavoriteMealModalVisible(false)}
+            onSetMealName={(newMealName: string) =>
+               setCurrentMealName(newMealName)
+            }
+         />
+
          <ItemForm
             onItemAdded={(foodItem: FoodItem) => {
                onItemAdded(foodItem);
             }}
          />
          <FoodItemTable foodItems={foodItems} />
+
+         <Text>{currentMealName}</Text>
 
          <Button onPress={onMealComplete}>Register Meal</Button>
       </View>
